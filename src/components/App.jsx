4ee6cdd744b6221fec8cf51bf677f1ecf74397d7
Priyanshu <psbd123@gmail.com>
2022-05-04 02:19:26 +0530

    [made footer]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import "../styles/index.css";
 import NavBar from "./navBar";
 import Home from "./homeTop";
 import HomeBottom from "./homebottom";
-
+import Footer from "./footer";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
       <HomeBottom />
+      <Footer />
     </div>
   );
 }
